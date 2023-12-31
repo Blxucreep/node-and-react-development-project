229bd84e91d25fa39092afed58169150cc860229
LambdaFr <148068047+LambdaFr@users.noreply.github.com>
2023-12-31 15:48:45 +0100

    Update study-now.component.ts

--- frontend-angular/src/app/study-now/study-now.component.ts
+++ frontend-angular/src/app/study-now/study-now.component.ts
@@ -1,40 +1,37 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { DatabaseService } from "../database.service";
+// Dans study-now.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-study-now',
   templateUrl: './study-now.component.html',
   styleUrls: ['./study-now.component.css']
 })
-export class StudyNowComponent implements OnInit, OnDestroy {
-  packages: any[] = [];
-  timerValue: number = 0;
-  intervalId: any;
+export class StudyNowComponent implements OnInit {
+  courses: any[] = [];
+  selectedCourseId: string = '';
+  packageDetails: any = null;
 
   constructor(private databaseService: DatabaseService) {}
 
   ngOnInit() {
-    this.fetchPackages();
-  }
-
-  fetchPackages() {
-    this.databaseService.getPackages().subscribe(
-      (response) => {
-        this.packages = response;
+    this.databaseService.getCourseTitles().subscribe(
+      data => {
+        this.courses = data;
       },
-      (error) => {
-        console.error('Error fetching packages:', error);
-      }
+      error => console.error('Error fetching course titles:', error)
     );
   }
 
-  startTimer() {
-    this.intervalId = setInterval(() => {
-      this.timerValue++;
-    }, 1000);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
+  onCourseSelected() {
+    if (this.selectedCourseId) {
+      this.databaseService.getPackageDetailsById(this.selectedCourseId).subscribe(
+        data => {
+          this.packageDetails = data;
+        },
+        error => console.error('Error fetching package details:', error)
+      );
+    }
   }
 }
