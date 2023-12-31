84a10b16442bd457034c37039de0e0b8e6117c9b
louise2701 <148068437+louise2701@users.noreply.github.com>
2023-12-31 12:07:34 +0100

    Update study-now.component.ts

--- frontend-angular/src/app/study-now/study-now.component.ts
+++ frontend-angular/src/app/study-now/study-now.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DatabaseService } from "../database.service";
 
 @Component({
   selector: 'app-study-now',
   templateUrl: './study-now.component.html',
   styleUrls: ['./study-now.component.css']
 })
-export class StudyNowComponent {
+export class StudyNowComponent implements OnInit, OnDestroy {
+  packages: any[] = [];
+  timerValue: number = 0;
+  intervalId: any;
 
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit() {
+    this.fetchPackages();
+  }
+
+  fetchPackages() {
+    this.databaseService.getPackages().subscribe(
+      (response) => {
+        this.packages = response;
+      },
+      (error) => {
+        console.error('Error fetching packages:', error);
+      }
+    );
+  }
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      this.timerValue++;
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
