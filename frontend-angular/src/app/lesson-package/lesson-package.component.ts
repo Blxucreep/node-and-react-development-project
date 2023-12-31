30e18df59429e6ca9aa22815ed1f83aaed2336ed
louise2701 <148068437+louise2701@users.noreply.github.com>
2023-12-31 12:08:56 +0100

    Add files via upload

---
+++ frontend-angular/src/app/lesson-package/lesson-package.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-package',
+  templateUrl: './lesson-package.component.html',
+  styleUrls: ['./lesson-package.component.css']
+})
+export class LessonPackageComponent {
+
+}
